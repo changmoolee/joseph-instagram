b278ec560c0f49f41d59b5004cff46f0a9da3286
Lee Changmoo <lcm8543@gmail.com>
2023-09-18 22:05:37 +0900

    Wip:[로그인 버튼] 컴포넌트 추가

---
+++ src/components/LoginButton/LoginButton.component.tsx
@@ -0,0 +1,10 @@
+/**
+ * 로그인 페이지로 이동하는 버튼 컴포넌트
+ */
+export default function LoginButton() {
+  return (
+    <button className="p-4 border-2 border-solid border-black rounded-lg">
+      Sign in with Google
+    </button>
+  );
+}
