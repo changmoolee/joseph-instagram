4fae752e609de57b2ced579626da6d87444be553
Lee Changmoo <lcm8543@gmail.com>
2023-10-19 22:26:44 +0900

    Wip:[댓글 input] 컴포넌트 진행중

---
+++ src/components/CommentInput/CommentInput.component.tsx
@@ -0,0 +1,16 @@
+import { BiSmile } from "react-icons/bi";
+
+/**
+ * 댓글을 입력할 수 있는 Input과 Post 버튼
+ */
+export default function CommentInput() {
+  return (
+    <section>
+      <button>
+        <BiSmile />
+      </button>
+      <input />
+      <button>Post</button>
+    </section>
+  );
+}
