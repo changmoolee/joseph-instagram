1e24385a308bdba22afbbcb91610451d8f06c9cc
Lee Changmoo <lcm8543@gmail.com>
2023-10-24 22:21:05 +0900

    Feat:[ColorButton] 컴포넌트 추가

---
+++ src/components/ColorButton/ColorButton.component.tsx
@@ -0,0 +1,26 @@
+interface IButtonProps {
+  /**
+   * 버튼에 들어갈 텍스트
+   */
+  text?: string;
+  /**
+   * className
+   */
+  className?: string;
+}
+
+/**
+ * Button 컴포넌트
+ */
+export default function ColorButton(props: IButtonProps) {
+  // props
+  const { text = "Button", className } = props;
+
+  return (
+    <button
+      className={`w-[200px] h-[50px] flex justify-center items-center text-white font-bold ${className}`}
+    >
+      {text}
+    </button>
+  );
+}
