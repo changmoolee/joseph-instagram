24b4552b07b0b1bcad7a89fbda5e994380dc6231
Lee Changmoo <lcm8543@gmail.com>
2023-11-12 17:47:24 +0900

    Feat:[ProfileAndName] 컴포넌트 추가

---
+++ src/components/ProfileAndName/ProfileAndName.component.tsx
@@ -0,0 +1,27 @@
+import ProfileImage from "@/components/ProfileImage/ProfileImage.component";
+
+interface IProfileAndNameProps {
+  /**
+   * 프로필 이미지 소스
+   */
+  src: string;
+  /**
+   * 프로필 이름
+   */
+  name: string;
+}
+
+/**
+ * ProfileAndName 컴포넌트
+ */
+export default function ProfileAndName(props: IProfileAndNameProps) {
+  // props
+  const { src, name } = props;
+
+  return (
+    <div className="flex items-center gap-5 p-2">
+      <ProfileImage src={src} />
+      <span className="font-bold">{name}</span>
+    </div>
+  );
+}
