9a840593bf5255b41d9a5005ced4662541203064
Lee Changmoo <lcm8543@gmail.com>
2023-11-13 22:30:11 +0900

    Feat:[new Post] 페이지 기본 스타일 완료

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,17 +1,16 @@
 import ColorButton from "@/components/ColorButton/ColorButton.component";
-import ProfileImage from "@/components/ProfileImage/ProfileImage.component";
+import DragAndDrop from "@/components/DragAndDrop/DragAndDrop.component";
+import ProfileAndName from "@/components/ProfileAndName/ProfileAndName.component";
+import Textarea from "@/components/Textarea/Textarea.component";
 
 export default function NewPost() {
   return (
     <main className="w-full h-full flex justify-center items-center">
       <section className="w-[600px] h-full flex flex-col items-center mt-5">
-        <div>
-          <ProfileImage src="/" />
-        </div>
-        <div>
-          <textarea placeholder="Write a caption..." />
-        </div>
-        <ColorButton text="Publish" />
+        <ProfileAndName src="/" name="wow" />
+        <DragAndDrop />
+        <Textarea placeholder="Write a capiton..." />
+        <ColorButton text="Publish" className="w-full h-[60px] bg-sky-400" />
       </section>
     </main>
   );
