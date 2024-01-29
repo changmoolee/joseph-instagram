e208ced5cdd98cd1de4ffd14e5aec87d95f5998f
Lee Changmoo <lcm8543@gmail.com>
2024-01-29 22:23:34 +0900

    Refactor:[로그인 페이지] react-hook-form으로 리팩토링

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
+import { useForm } from "react-hook-form";
 
 interface LoginState {
   isLogin: boolean;
@@ -14,7 +15,7 @@ interface LoginState {
 }
 
 /** 로그인 전역상태 */
-const useLoginStateStore = create<LoginState>()(
+export const useLoginStateStore = create<LoginState>()(
   persist(
     (set) => ({
       isLogin: false,
@@ -37,17 +38,22 @@ export default function Login() {
   /** router */
   const router = useRouter();
 
-  const excuteLogin = useLoginStateStore((state) => state.excuteLogin);
+  const {
+    register,
+    setValue,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  // 메일주소
-  const [email, setEmail] = React.useState<string>("");
-  // 비밀번호
-  const [password, setPassword] = React.useState<string>("");
+  const excuteLogin = useLoginStateStore((state) => state.excuteLogin);
 
   /**
    * 로그인 함수
    */
-  const signIn = async () => {
+  const signIn = async (data: any) => {
+    // 객체분해할당
+    const { email, password } = data;
+
     const response = await axios.post("/api/auth/sign-in", {
       email,
       password,
@@ -66,43 +72,55 @@ export default function Login() {
       // 에러메시지
       alert(message);
       // 비밀번호 초기화
-      setPassword("");
+      setValue("password", "");
     }
+  };
+
+  // 유효성 검사 통과시 실행될 함수
+  const onSubmit = (data: any) => {
+    signIn(data);
   };
 
   return (
     <main className="w-full h-full flex flex-col justify-center items-center">
-      <section className="w-[600px] flex flex-col gap-5">
-        <article className="w-full flex items-center mt-5 gap-5">
-          <span className="w-[80px]">이메일</span>
-          <input
-            className="w-full"
-            placeholder="abc1234@gmail.com"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
+      <section className="w-full flex justify-center">
+        <span className="font-[600]">로그인</span>
+      </section>
+      <form
+        className="w-[600px] flex flex-col gap-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <article className="w-full mt-5 gap-5">
+          <section className="w-full flex">
+            <span className="w-[120px]">이메일</span>
+            <input
+              className="w-full"
+              placeholder="abc1234@gmail.com"
+              {...register("email", { required: true })}
+            />
+          </section>
+          {errors.email && (
+            <span className="text-[red]">이메일을 입력해 주세요.</span>
+          )}
         </article>
-        <article className="w-full flex items-center mt-5 gap-5">
-          <span className="w-[80px]">비밀번호</span>
-          <input
-            className="w-full"
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
+        <article className="w-full mt-5 gap-5">
+          <section className="w-full flex">
+            <span className="w-[120px]">비밀번호</span>
+            <input
+              className="w-full"
+              type="password"
+              placeholder="password"
+              {...register("password", { required: true })}
+            />
+          </section>
+          {errors.password && (
+            <span className="text-[red]">비밀번호를 입력해 주세요.</span>
+          )}
         </article>
-        <button
-          className="w-full flex justify-center items-center text-[#fff] font-[600] bg-blue-500"
-          onClick={signIn}
-        >
+        <button className="w-full flex justify-center items-center text-[#fff] font-[600] bg-blue-500">
           로그인
         </button>
-      </section>
+      </form>
     </main>
   );
 }
