6b3a586e0872cd810665258321ec2f16035428da
Lee Changmoo <lcm8543@gmail.com>
2024-02-03 00:06:29 +0900

    Fix:[DragAndDrop 컴포넌트] label요소 원복

--- src/components/DragAndDrop/DragAndDrop.component.tsx
+++ src/components/DragAndDrop/DragAndDrop.component.tsx
@@ -66,29 +66,31 @@ export default function DragAndDrop(props: IDragAndDropProps) {
       onDrop={handleDrop}
       className="w-[600px] h-[300px]"
     >
-      {selectedFiles.length === 0 ? (
-        <div className="w-full h-full flex flex-col justify-center items-center gap-3 bg-white border-dotted border-sky-400 border-4">
-          <input
-            id="drag-and-drop"
-            type="file"
-            accept="image/*"
-            multiple
-            style={{ display: "none" }}
-            onChange={handleFileInputChange}
-            ref={inputRef}
-          />
-          <FaPhotoVideo className="w-[30%] h-[30%]" color="#ced4da" />
-          Drag and Drop your image here or click
-        </div>
-      ) : (
-        <Image
-          src={previewImgSrc}
-          alt="profile-image"
-          width={100}
-          height={100}
-          className="w-full h-full object-contain"
+      <label htmlFor="drag-and-drop" className="w-full h-full">
+        <input
+          id="drag-and-drop"
+          type="file"
+          accept="image/*"
+          multiple
+          style={{ display: "none" }}
+          onChange={handleFileInputChange}
+          ref={inputRef}
         />
-      )}
+        {selectedFiles.length === 0 ? (
+          <div className="w-full h-full flex flex-col justify-center items-center gap-3 bg-white border-dotted border-sky-400 border-4">
+            <FaPhotoVideo className="w-[30%] h-[30%]" color="#ced4da" />
+            Drag and Drop your image here or click
+          </div>
+        ) : (
+          <Image
+            src={previewImgSrc}
+            alt="profile-image"
+            width={100}
+            height={100}
+            className="w-full h-full object-contain"
+          />
+        )}
+      </label>
       <div>
         {selectedFiles.map((file, index) => (
           <div key={index}>{file.name}</div>
