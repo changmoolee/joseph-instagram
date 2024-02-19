974bd3f1e10e52a43965027f9ce8bcd4f8c97071
Lee Changmoo <lcm8543@gmail.com>
2024-02-19 23:35:29 +0900

    Update:[ProfileCard 컴포넌트] props 이름 및 정리

--- src/components/ProfileCard/ProfileCard.component.tsx
+++ src/components/ProfileCard/ProfileCard.component.tsx
@@ -2,9 +2,7 @@ import ProfileImage from "@/components/ProfileImage/ProfileImage.component";
 
 interface IProfileCardProps {
   /** 영어 이름 */
-  engName?: string;
-  /** 한국 이름 */
-  korName?: string;
+  name?: string;
   /** 필로워 수 */
   followersNum?: number;
   /** 필로잉 수 */
@@ -16,19 +14,13 @@ interface IProfileCardProps {
  */
 export default function ProfileCard(props: IProfileCardProps) {
   // props
-  const {
-    engName = "Eng name",
-    korName = "홍길동",
-    followersNum = 0,
-    followingNum = 0,
-  } = props;
+  const { name, followersNum, followingNum } = props;
 
   return (
     <article className="flex items-center gap-3 p-5 border-solid border-[2px] border-gray-200">
       <ProfileImage src="/" />
       <section className="flex flex-col">
-        <span className="text-black fond-bold">{engName}</span>
-        <span className="text-gray-400 fond-bold">{korName}</span>
+        <span className="text-black fond-bold">{name}</span>
         <span className="text-gray-400 fond-bold">{`${followersNum} followers ${followingNum} following`}</span>
       </section>
     </article>
