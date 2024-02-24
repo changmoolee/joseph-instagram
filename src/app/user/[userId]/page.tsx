e75aacb6835349312a1c8992989bcea37db9798f
Lee Changmoo <lcm8543@gmail.com>
2024-02-24 23:00:37 +0900

    Update:[user페이지] 1) 상태이름 및 타입 수정 2) tab 클릭에 따라 api params 분기 호출

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -4,33 +4,42 @@ import ColorButton from "@/components/ColorButton/ColorButton.component";
 import BigProfileImage from "@/components/ProfileImage/BigProfileImage.component";
 import Tab from "@/components/Tab/Tab.component";
 import axios from "axios";
+import Image from "next/image";
 import React from "react";
 
-interface IUserData {
+interface IPostData {
+  CreateDate: string;
+  description: string;
+  image: string;
+  like_user: string[];
+  userId: string;
   _id: string;
-  email: string;
-  name: string;
 }
 
 export default function User({ params }: { params: { userId: string } }) {
   const tabs = ["POSTS", "SAVED", "LIKED"];
 
-  const [userData, setUserData] = React.useState<IUserData>();
+  // 클릭한 탭의 index
+  const [clickedTab, setClickedTab] = React.useState<string>(tabs[0]);
+
+  const [postData, setPostData] = React.useState<IPostData[]>([]);
 
   /**
    * 유저 개인의 포스트 데이터 호출
    */
-  const getUserData = async (userId: string) => {
+  const getPostData = async (userId: string, clickedTab: string) => {
     // 객체분해할당
     const response = await axios.get<{
-      data: IUserData;
+      data: IPostData[];
       result: string;
       message: string;
-    }>(`/api/user/${userId}`);
+    }>(`/api/user/${userId}`, {
+      params: { post: clickedTab.toLocaleLowerCase() },
+    });
 
     const { result, data, message } = response.data;
     if (result === "success") {
-      setUserData(data);
+      setPostData(data);
     }
     if (result === "fail") {
       // 에러메시지
@@ -39,8 +48,8 @@ export default function User({ params }: { params: { userId: string } }) {
   };
 
   React.useEffect(() => {
-    getUserData(params.userId);
-  }, [params.userId]);
+    getPostData(params.userId, clickedTab);
+  }, [params.userId, clickedTab]);
 
   return (
     <main className="w-full h-full flex flex-col items-center">
@@ -70,7 +79,22 @@ export default function User({ params }: { params: { userId: string } }) {
           </article>
         </section>
       </div>
-      <Tab tabArr={tabs} />
+      <Tab
+        tabArr={tabs}
+        onChange={(selectedTab) => {
+          setClickedTab(selectedTab);
+        }}
+      />
+      <ul className="max-w-[1000px] w-full h-full grid grid-cols-3 gap-4">
+        {postData.map((data) => (
+          <li
+            key={data._id}
+            className="relative w-full h-auto aspect-[1/1] bg-red-500 mt-12"
+          >
+            <Image src="/" alt="post-image" fill />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
