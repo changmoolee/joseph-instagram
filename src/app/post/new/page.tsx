a98346c4c4242d9d7cf7263eb889c86ce1aa28c9
Lee Changmoo <lcm8543@gmail.com>
2024-02-28 23:15:37 +0900

    Update:[new Post] s3로 이미지 업로드 기능 추가

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -20,25 +20,44 @@ export default function NewPost() {
    * 게시물(post) 등록 함수
    */
   const postCreate = async () => {
-    console.log("imageFile", imageFile);
-    console.log("description", description);
+    if (!imageFile || imageFile.length === 0) {
+      alert("등록할 이미지가 없습니다.");
+      return;
+    }
 
-    // const response = await axios.post("/api/post", {
-    //   imageFile,
-    //   description,
-    // });
+    const response = await axios.post("/api/upload", {
+      imageInfo: JSON.stringify({
+        filename: imageFile[0].name,
+        contentType: imageFile[0].type,
+      }),
+      description,
+    });
 
-    // const { result, message } = response.data;
+    const { result, data, message } = response.data;
 
-    // if (result === "success") {
-    //   // 메인페이지로 이동
-    //   router.push("/");
-    // }
+    if (result === "success") {
+      const { url, fields } = data;
 
-    // if (result === "fail") {
-    //   // 에러메시지
-    //   alert(message);
-    // }
+      const formData = new FormData();
+      Object.entries(fields).forEach(([key, value]) => {
+        formData.append(key, value as string);
+      });
+      formData.append("file", imageFile[0]);
+
+      try {
+        await axios.post(url, formData);
+
+        alert("게시물이 정상적으로 업로드되었습니다.");
+        router.push("/");
+      } catch (error: any) {
+        alert(error.message);
+      }
+    }
+
+    if (result === "fail") {
+      // 에러메시지
+      alert(message);
+    }
   };
 
   return (
