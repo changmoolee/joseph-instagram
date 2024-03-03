9d4342b3e868ba3b38e17c0a49995b3f04f6e731
Lee Changmoo <lcm8543@gmail.com>
2024-03-03 16:50:52 +0900

    Update:[공통 인터페이스 추가] ICommonResponse 인터페이스

--- utils/upload.ts
+++ utils/upload.ts
@@ -1,24 +1,38 @@
 import axios from "axios";
+import {
+  ICommonResponse,
+  ICommonReturn,
+} from "../typescript/common/response.interface";
+
+interface IUploadData {
+  url: string;
+  fields: Record<string, string>;
+}
 
 /**
  * S3 이미지 업로드 함수
  */
-export const ImageUpload = async (imageFile: File[]) => {
+export const ImageUpload = async (
+  imageFile: File[]
+): Promise<ICommonReturn<string>> => {
   if (!imageFile || imageFile.length === 0) {
     alert("등록할 이미지가 없습니다.");
-    return;
+    return { result: "fail" };
   }
 
-  const response = await axios.post("/api/upload", {
-    imageInfo: JSON.stringify({
-      filename: imageFile[0].name,
-      contentType: imageFile[0].type,
-    }),
-  });
+  const response: ICommonResponse<IUploadData> = await axios.post(
+    "/api/upload",
+    {
+      imageInfo: JSON.stringify({
+        filename: imageFile[0].name,
+        contentType: imageFile[0].type,
+      }),
+    }
+  );
 
   const { result, data, message } = response.data;
 
-  if (result === "success") {
+  if (result === "success" && data) {
     const { url, fields } = data;
 
     const formData = new FormData();
@@ -30,18 +44,14 @@ export const ImageUpload = async (imageFile: File[]) => {
     try {
       await axios.post(url, formData);
 
-      console.log("image-url", url + fields.key);
-
-      return { url: url + fields.key };
+      return { result: "success", data: url + fields.key };
     } catch (error: any) {
       alert(error.message);
-      return;
+      return { result: "fail", data: null };
     }
-  }
-
-  if (result === "fail") {
+  } else {
     // 에러메시지
     alert(message);
-    return;
+    return { result: "fail", data: null };
   }
 };
