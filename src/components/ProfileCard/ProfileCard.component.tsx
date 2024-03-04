fce5991646e0ccd99df028a994995789a46fc643
Lee Changmoo <lcm8543@gmail.com>
2024-03-04 22:05:32 +0900

    Update:[ProfileCard] image props 추가

--- src/components/ProfileCard/ProfileCard.component.tsx
+++ src/components/ProfileCard/ProfileCard.component.tsx
@@ -1,6 +1,8 @@
 import ProfileImage from "@/components/ProfileImage/ProfileImage.component";
 
 interface IProfileCardProps {
+  /** 프로필 이미지 */
+  image?: string;
   /** 영어 이름 */
   name?: string;
   /** 필로워 수 */
@@ -14,11 +16,11 @@ interface IProfileCardProps {
  */
 export default function ProfileCard(props: IProfileCardProps) {
   // props
-  const { name, followersNum, followingNum } = props;
+  const { image, name, followersNum, followingNum } = props;
 
   return (
     <article className="flex items-center gap-3 p-5 border-solid border-[2px] border-gray-200">
-      <ProfileImage src="/" />
+      <ProfileImage src={image} />
       <section className="flex flex-col">
         <span className="text-black fond-bold">{name}</span>
         <span className="text-gray-400 fond-bold">{`${followersNum} followers ${followingNum} following`}</span>
