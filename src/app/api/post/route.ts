fb4b8a0534024b277862d9708efd41bad78df826
Lee Changmoo <lcm8543@gmail.com>
2024-03-14 23:39:46 +0900

    Feat:[게시물 조회 api] 메인페이지 게시물 조회 - 무한스크롤 구현 예정

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,6 +2,45 @@ import dayjs from "dayjs";
 import { connectToDatabase } from "@/utils/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
+/** 게시물 조회 */
+export async function GET(req: NextRequest) {
+  const { client } = await connectToDatabase();
+
+  const db = client.db("sample_mflix");
+
+  const { skip } = await req.json();
+
+  /** 한 번에 가져올 데이터 개수 */
+  const pageSize = 10;
+
+  const currentSkip = parseInt(skip) || 0;
+
+  // 데이터 추가
+  const postsData = await db
+    .collection("posts")
+    .find()
+    .sort({
+      CreateDate: -1, // 내림차순(최신)
+    })
+    .skip(currentSkip)
+    .limit(pageSize)
+    .toArray();
+
+  try {
+    return NextResponse.json({
+      data: postsData,
+      result: "success",
+      message: "게시물 등록을 성공하였습니다.",
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      result: "fail",
+      message: error.message,
+    });
+  }
+}
+
+/** 게시물 등록 */
 export async function POST(req: NextRequest) {
   const { image, description } = await req.json();
 
