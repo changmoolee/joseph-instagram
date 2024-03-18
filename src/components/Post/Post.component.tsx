df8ace7d8bb2f8670fea17c8051d040764354ccd
Lee Changmoo <lcm8543@gmail.com>
2024-03-18 22:29:49 +0900

    Update:[게시물 Post 등 컴포넌트] props 인터페이스 수정

--- src/components/Post/Post.component.tsx
+++ src/components/Post/Post.component.tsx
@@ -7,24 +7,22 @@ import PostModal from "@/components/PostModal/PostModal.component";
 import React from "react";
 
 interface IPostProps {
-  data: {
-    /** 프로필 이미지 */
-    profileSrc: string;
-    /** 작성자 이름 */
-    writerName: string;
-    /** 게시글 이미지 */
-    postSrc: string;
-    /** 좋아요 여부 */
-    like: boolean;
-    /** 북마크 여부 */
-    bookmark: boolean;
-    /** 좋아요 개수 */
-    likeNumber: number;
-    /** 게시글 내용 */
-    content: string;
-    /** 게시글 생성 날짜 */
-    createDate: string;
-  };
+  /** 프로필 이미지 */
+  profileSrc?: string;
+  /** 작성자 이름 */
+  username: string;
+  /** 게시글 이미지 */
+  postSrc?: string;
+  /** 좋아요 여부 */
+  like?: boolean;
+  /** 북마크 여부 */
+  bookmark?: boolean;
+  /** 좋아요 개수 */
+  likeNumber: number;
+  /** 게시글 내용 */
+  description: string;
+  /** 게시글 생성 날짜 */
+  createDate: string;
 }
 
 /**
@@ -32,35 +30,54 @@ interface IPostProps {
  */
 export default function Post(props: IPostProps) {
   // props
-  const { data } = props;
+  const {
+    /** 프로필 이미지 */
+    profileSrc,
+    /** 작성자 이름 */
+    username,
+    /** 게시글 이미지 */
+    postSrc,
+    /** 좋아요 여부 */
+    like,
+    /** 북마크 여부 */
+    bookmark,
+    /** 좋아요 개수 */
+    likeNumber,
+    /** 게시글 내용 */
+    description,
+    /** 게시글 생성 날짜 */
+    createDate,
+  } = props;
 
   const [open, setOpen] = React.useState<boolean>(false);
 
   return (
     <div className="w-[500px]">
       <section className="border-solid border-[1px] border-gray-200">
-        <ProfileAndName src={data?.profileSrc} name={data?.writerName} />
+        <ProfileAndName src={profileSrc} name={username} />
       </section>
-      <Image
-        src={data?.postSrc}
-        alt=""
-        className="w-full h-[400px]"
-        width={100}
-        height={100}
-      />
+      {postSrc && (
+        <Image
+          src={postSrc}
+          alt=""
+          className="w-full h-[400px]"
+          width={100}
+          height={100}
+        />
+      )}
       <section>
         <section className="w-full flex flex-col gap-2 p-2 border-solid border-[1px] border-gray-200">
           <div className="w-full h-[30px] flex justify-between">
-            <Like checked={data?.like} />
-            <Bookmark checked={data?.bookmark} />
+            {like && <Like checked={like} />}
+            {bookmark && <Bookmark checked={bookmark} />}
           </div>
           <div className="flex flex-col gap-2">
-            <span>{data?.likeNumber} Like</span>
+            <span>{likeNumber} Like</span>
             <div className="flex gap-2">
-              <span>{data?.writerName}</span>
-              <span>{data?.content}</span>
+              <span>{username}</span>
+              <span>{description}</span>
             </div>
-            <span className="text-gray-400">{data?.createDate} hours ago</span>
+            <span className="text-gray-400">{createDate} hours ago</span>
           </div>
         </section>
         <CommentInput onClick={() => setOpen(true)} />
