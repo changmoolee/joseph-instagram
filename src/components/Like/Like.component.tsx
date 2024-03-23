a8530096f7d5070624c10ee5ee1c97a86fc1ab51
Lee Changmoo <lcm8543@gmail.com>
2024-03-23 16:30:34 +0900

    Update:[Bookmark, Like 컴포넌트] size props 추가

--- src/components/Like/Like.component.tsx
+++ src/components/Like/Like.component.tsx
@@ -6,11 +6,22 @@ interface ILikeProps {
    * 좋아요 체크 유무
    */
   checked: boolean;
+  /**
+   * 사이즈 조절
+   */
+  size?: number;
 }
 
 /**
  * 좋아요 컴포넌트
  */
-export default function Like({ checked }: ILikeProps) {
-  return <button>{checked ? <AiFillHeart /> : <AiOutlineHeart />}</button>;
+export default function Like(props: ILikeProps) {
+  // props
+  const { checked, size } = props;
+
+  return (
+    <button>
+      {checked ? <AiFillHeart size={size} /> : <AiOutlineHeart size={size} />}
+    </button>
+  );
 }
