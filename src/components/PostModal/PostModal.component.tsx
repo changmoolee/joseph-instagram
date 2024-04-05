59589e009df6904707b4c88a04d597dc5ec3f442
Lee Changmoo <lcm8543@gmail.com>
2024-04-05 21:48:48 +0900

    Feat:[게시물 모달] 좋아요 기능 추가 및 회원정보 추가

--- src/components/PostModal/PostModal.component.tsx
+++ src/components/PostModal/PostModal.component.tsx
@@ -8,6 +8,8 @@ import ProfileAndName from "@/components/ProfileAndName/ProfileAndName.component
 import Image from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { excuteLike } from "@/utils/services/like";
+import { IUserData } from "@/typescript/user.interface";
 
 // dayjs의 RelativeTime 플러그인 추가
 dayjs.extend(relativeTime);
@@ -25,6 +27,10 @@ interface IPostModalProps {
    * Post 컴포넌트의 props
    */
   PostProps: IPostProps;
+  /**
+   * 로그인한 회원의 프로필 데이터
+   */
+  userInfo: IUserData | null;
 }
 
 /**
@@ -33,40 +39,69 @@ interface IPostModalProps {
  */
 export default function PostModal(props: IPostModalProps) {
   // props
-  const { open, onClose, PostProps } = props;
+  const { open, onClose, PostProps, userInfo } = props;
+
+  // props
+  const {
+    /** 게시글 Idx */
+    _id: postId,
+    /** 게시글 이미지 */
+    image: postSrc,
+    /** 게시글 내용 */
+    description,
+    /** 게시글 생성 날짜 */
+    CreateDate: createDate,
+    /** 북마크 여부 */
+    bookmark,
+    /** 회원 정보 */
+    userDetails,
+    /** 좋아요 정보 */
+    likeDetails,
+  } = PostProps;
 
   return (
     <Modal open={open} onClose={onClose}>
       <section className="w-[800px] h-[500px] flex bg-white">
         <div className="relative w-[60%] h-full bg-black">
           <Image
-            src={PostProps.postSrc || "/"}
+            src={postSrc || "/"}
             alt="post-image"
             className="object-cover"
             fill
           />
         </div>
         <div className="relative w-[40%] h-full">
-          <section className="flex">
+          <section className="flex flex-col">
             <ProfileAndName
-              src={PostProps.profileSrc}
-              name={PostProps.username}
+              src={userDetails.at(0)?.image}
+              name={userDetails.at(0)?.name || ""}
             />
+            <p className="px-5 py-2">{description}</p>
           </section>
-          <section>
-            <Comment />
-            <Comment />
+          <section className="h-[260px] p-2 overflow-y-auto">
             <Comment />
           </section>
           <section className="absolute bottom-0 w-full flex-col">
             <div className="flex justify-between px-2 py-1">
-              <Like checked size={20} />
+              <Like
+                checked={
+                  !!likeDetails.find((like) => like.userId === userInfo?._id)
+                }
+                size={20}
+                onClick={() =>
+                  excuteLike({
+                    likeDetails,
+                    userId: userInfo?._id || null,
+                    postId,
+                  })
+                }
+              />
               <Bookmark checked size={20} />
             </div>
             <div className="flex flex-col gap-2 p-2">
-              <span>{PostProps.likeNumber} Like</span>
+              <span>{likeDetails.length} Like</span>
               <span className="text-gray-400">
-                {dayjs(PostProps.createDate).fromNow()}
+                {dayjs(createDate).fromNow()}
               </span>
             </div>
             <CommentInput />
