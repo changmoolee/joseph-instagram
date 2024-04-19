1551b5083356180392c4c69dfd0fc28c8f3cd464
Lee Changmoo <lcm8543@gmail.com>
2024-04-19 14:06:28 +0900

    Update:[메인 등 3페이지] 추가된 custom hook으로 교체 및 useEffect로 성공여부 확인

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { ICommonResponse } from "@/typescript/common/response.interface";
 import apiClient from "@/utils/axios";
-import { IUserData } from "@/typescript/user.interface";
+import { useGetMyData } from "@/hooks/user/useGetMyData";
 
 /**
  * 마이 페이지
@@ -16,8 +16,6 @@ import { IUserData } from "@/typescript/user.interface";
 export default function MyPage() {
   // router
   const router = useRouter();
-
-  const [userInfo, setUserInfo] = React.useState<IUserData>();
 
   /**
    * 회원 탈퇴
@@ -49,36 +47,14 @@ export default function MyPage() {
     }
   };
 
-  /**
-   * 유저 개인 데이터 호출
-   */
-  const getUserData = async () => {
-    // 객체분해할당
-
-    const response: ICommonResponse<IUserData> = await apiClient.get(
-      `/api/user/my-page`,
-      {
-        withCredentials: true,
-      }
-    );
-
-    const { result, data, message } = response.data;
-
-    if (result === "success" && data) {
-      // 성공시 데이터 상태저장
-      setUserInfo(data);
-    }
-
-    if (result === "fail") {
-      // 에러메시지
-      alert(message);
-    }
-  };
+  const { data: userInfo, error, message } = useGetMyData();
 
   React.useEffect(() => {
-    // 유저 프로필 데이터 호출
-    getUserData();
-  }, []);
+    if (error) {
+      alert(message);
+      router.push("/");
+    }
+  }, [error, message, router]);
 
   return (
     userInfo && (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,29 @@
 "use client";
 
 import Post from "@/components/Post/Post.component";
-import ProfileAboveName from "@/components/ProfileAboveName/ProfileAboveName.component";
 import ProfileAndName from "@/components/ProfileAndName/ProfileAndName.component";
+import { useGetPost } from "@/hooks/post/useGetPost";
 import { useLoginStore } from "@/store/useLoginStore";
-import { ICommonResponse } from "@/typescript/common/response.interface";
-import { IPostData } from "@/typescript/post.interface";
-import apiClient from "@/utils/axios";
-import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  const [skip, setSkip] = React.useState<number>(1);
-  const [postData, setPostData] = React.useState<IPostData[]>([]);
+  // const [skip, setSkip] = React.useState<number>(1);
 
-  /** 게시물 데이터 */
-  const getPostData = async () => {
-    const response: ICommonResponse<IPostData[]> = await apiClient.get(
-      "/api/post",
-      {}
-    );
-
-    const { result, data, message } = response.data;
-
-    if (result === "success" && data) {
-      setPostData(data);
-    }
-
-    if (result === "fail") {
-      alert(message || "게시물을 불러오지 못했습니다.");
-    }
-  };
+  const { data: postData } = useGetPost();
 
   /** 유저 개인 프로필 전역 상태 데이터 */
   const userInfo = useLoginStore((state) => state.userInfo);
-
-  React.useEffect(() => {
-    getPostData();
-  }, [skip]);
 
   return (
     <main className="w-full h-full flex justify-center">
       <section className="w-[500px] h-full">
         <section className="w-full flex gap-5 border-[2px] p-5 border-gray-100 border-box"></section>
         {/* 친구들이 올린 post 데이터 내림차순 */}
-        {postData.map((post) => (
-          <Post key={post._id} {...post} />
-        ))}
+        {postData.length > 0 ? (
+          postData.map((post) => <Post key={post._id} {...post} />)
+        ) : (
+          <span>게시물이 없습니다.</span>
+        )}
       </section>
       <section className="w-[200px] h-full flex flex-col gap-5 p-5">
         {userInfo && (
