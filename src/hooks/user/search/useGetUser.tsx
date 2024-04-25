db9b649efc9e33ddfef97b6a9966a74358d3a83a
Lee Changmoo <lcm8543@gmail.com>
2024-04-26 00:16:25 +0900

    Wip:[회원검색 api] swr로 리팩토링 - 타입 가드 및 조건문 수정

--- src/hooks/user/search/useGetUser.tsx
+++ src/hooks/user/search/useGetUser.tsx
@@ -13,28 +13,28 @@ export function useGetUser(searchWord: string) {
 
   const {
     data: userResponse,
+    error,
     isLoading,
     mutate,
   } = useSWR<ICommonResponse<IUserData[]>>(urlKey, fetcher);
 
-  if (userResponse) {
-    const { data, result, message } = userResponse.data;
-
-    if (result === "success") {
-      return {
-        data,
-        isLoading,
-        message,
-        mutate,
-      };
-    }
+  // api 호출 성공시
+  if (userResponse?.data) {
+    const { data, message } = userResponse.data;
 
     return {
       data,
-      error: new Error("나의 프로필 데이터를 불러오지 못했습니다."),
       isLoading,
       message,
       mutate,
     };
   }
+
+  return {
+    data: [],
+    error,
+    isLoading,
+    message: error?.message || "나의 프로필 데이터를 불러오지 못했습니다.",
+    mutate,
+  };
 }
