9620d0a279dd2636b4cb8980eb0f8e25670bcefd
Lee Changmoo <lcm8543@gmail.com>
2025-03-05 18:36:08 +0900

    Update:[Header] 1. gap, padding 조절 2. 텍스트 수정 3. 프로필 이미지 url 입력

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -10,6 +10,7 @@ import { useLoginStore } from "@/store/useLoginStore";
 import apiClient from "@/utils/axios";
 import { useCheckLogin } from "@/hooks/auth/useCheckLogin";
 import React from "react";
+import ProfileImage from "@/components/ProfileImage/ProfileImage.component";
 
 /**
  * 헤더 컴포넌트
@@ -18,7 +19,11 @@ export default function Header() {
   /** router */
   const router = useRouter();
 
+  /** 로그인 유무 */
   const isLogin = useLoginStore((state) => state.isLogin);
+
+  /** 유저 개인 프로필 전역 상태 데이터 */
+  const userInfo = useLoginStore((state) => state.userInfo);
 
   const excuteLogout = useLoginStore((state) => state.excuteLogout);
 
@@ -62,34 +67,31 @@ export default function Header() {
           Instargram
         </Link>
 
-        <ul className="flex h-full items-center gap-x-5">
-          <li>
-            <Link href={"/"}>
-              <AiOutlineHome className="hidden h-[25px] w-[25px] lg:block" />
-            </Link>
-          </li>
+        <ul className="flex h-full items-center gap-x-[15px] lg:gap-x-[30px]">
           <li>
             <Link href={"/search"}>
-              <FiSearch className="h-[25px] w-[25px]" />
+              <FiSearch className="h-[20px] w-[20px] lg:h-[25px] lg:w-[25px]" />
             </Link>
           </li>
           {isLogin && (
             <li>
               <Link href={"/post/new"}>
-                <BsPlusSquare className="h-[25px] w-[25px]" />
+                <BsPlusSquare className="h-[20px] w-[20px] lg:h-[25px] lg:w-[25px]" />
               </Link>
             </li>
           )}
           {isLogin && (
             <li>
-              <Link href={"/my-page"}>Profile Image</Link>
+              <Link href={"/my-page"}>
+                <ProfileImage src={userInfo?.image_url} />
+              </Link>
             </li>
           )}
-          <li>
+          <li className="text-[14px]">
             {isLogin ? (
-              <button onClick={signOut}>Sign out</button>
+              <button onClick={signOut}>로그아웃</button>
             ) : (
-              <Link href={"/login"}>Sign in</Link>
+              <Link href={"/login"}>로그인</Link>
             )}
           </li>
         </ul>
